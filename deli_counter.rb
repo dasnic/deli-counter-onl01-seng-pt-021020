bf9948b94d3c9ac1001b5bc4b15c0d9c16d60bdf
Lydell Gaines <dasnic70@gmail.com>
2020-02-20 03:53:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,5 @@
+var katzDeli = [];
+
 function takeANumber(currentLine, name){
 var placeInLine = currentLine.length + 1;
 var outputString = “Welcome, ” + name +”. You are number ” + placeInLine + ” in line.”;
