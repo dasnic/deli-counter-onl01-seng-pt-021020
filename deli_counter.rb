679fdca09e5ee44f04581fb0347a9c29c012b8f1
Lydell Gaines <dasnic70@gmail.com>
2020-02-20 03:57:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,54 @@
-var katzDeli = [];
+katz_deli = ["Moshe", "Fayge", "Rivki"]
 
-function takeANumber(currentLine, name){
-var placeInLine = currentLine.length + 1;
-var outputString = “Welcome, ” + name +”. You are number ” + placeInLine + ” in line.”;
-currentLine.push(name);
-return outputString;
-}
 
-function nowServing(currentLine){
-var outputString;
-if (currentLine.length === 0){
-outputString = “There is nobody waiting to be served!”
-}
-else{
-outputString = “Currently serving ” + currentLine[0] + “.”;
-currentLine.shift();
-}
-return outputString;
-}
+def line(array) # this was the one I figured out
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
 
-function currentLine(line){
-var outputString;
-if (line.length === 0){
-outputString = “The line is currently empty.”
-}
-else{
-outputString = “The line is currently: ”
-for(var i = 0; i < line.length ; i++){
-if (i === 0){
+line(katz_deli)
+
+def line_simple(array) # this one follows the same mechanics as learn.co
+  current_line = "The simple line is currently:"
+  array.each.with_index(1) do |value, indexemus|  
+  # "each.with_index" is the method...must use "index"
+    current_line << " #{indexemus}. #{value},"    
+    # "indexemus" is used to illustrate this variable can be different from "index"
+  end 
+  puts current_line
+end 
+  
+line_simple(katz_deli)  
+#[:foo, :bar, :baz].each.with_index(1) do |value, index|
+#    puts "#{index}: #{value}"
+#end
+
+
+
+def take_a_number(line, new_person)
+  line.push(new_person) # could say: "line << new_person"
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end
+
+take_a_number(katz_deli, "Fyvish")
+
+def now_serving(line)
+  if line.length == 0 # could say: "if deli.empty?"
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
+    line.shift # this works in the IDE but no on repl.it
+  end
+end
+
+puts now_serving(katz_deli)
+puts katz_deli
